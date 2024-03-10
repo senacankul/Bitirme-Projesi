8a9240a5dcd1b3a2c943ebc94659e202954837ed
Sena Cankul <sena@gmail.com>
2024-03-10 19:21:21 +0300

    vercel update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    cors: false, // Enable CORS
+    cors: false,
     proxy: {
       '/api': {
         target: 'https://acikyesil.bursa.bel.tr/api/3/action/datastore_search?resource_id=1b8c32eb-d586-4d5c-ad6c-ebcb9e856496',
