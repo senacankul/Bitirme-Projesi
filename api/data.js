bcef5ceba362158ccf0e848fcf2ce8f61aaace2f
Sena Cankul <sena@gmail.com>
2024-03-10 19:43:35 +0300

    data update

--- api/data.js
+++ api/data.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 export default async function handler(req, res) {
   try {
     process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-    const response = await fetch('https://acikyesil.bursa.bel.tr/api/3/action/datastore_search?resource_id=1b8c32eb-d586-4d5c-ad6c-ebcb9e856496?limit=20');
+    const response = await fetch('https://acikyesil.bursa.bel.tr/api/3/action/datastore_search?resource_id=1b8c32eb-d586-4d5c-ad6c-ebcb9e856496&limit=20');
     const data = await response.json();
     res.json(data);
   } catch (error) {
