53ee3d88e14a4db417cd1a0eae2ecc62663aa9e4
Sena Cankul <sena@gmail.com>
2024-03-10 19:35:28 +0300

    vercel update

---
+++ api/data.js
@@ -0,0 +1,13 @@
+// api/data.js
+import fetch from 'node-fetch';
+
+export default async function handler(req, res) {
+  try {
+    const response = await fetch('https://acikyesil.bursa.bel.tr/api/3/action/datastore_search?resource_id=1b8c32eb-d586-4d5c-ad6c-ebcb9e856496&limit=5');
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Error fetching data' });
+  }
+}
